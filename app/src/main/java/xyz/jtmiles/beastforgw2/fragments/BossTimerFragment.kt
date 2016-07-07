77d97db2797f87485df9b313d992f44da0842bc6
DarkenedFate <darkenedfate92@gmail.com>
2016-07-06 20:18:36 -0400

    Since I apparently I did not use the right words, the previous commit fixes issue #3.  This commit adds a log statement.

--- app/src/main/java/xyz/jtmiles/beastforgw2/fragments/BossTimerFragment.kt
+++ app/src/main/java/xyz/jtmiles/beastforgw2/fragments/BossTimerFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -65,7 +66,7 @@ class BossTimerFragment : Fragment() {
 
 
             } catch (ex: Exception){
-
+                Log.w("BossTimerFragment", "Error loading bosses: ${ex.message}")
             }
 
             val mapper = jacksonObjectMapper()
